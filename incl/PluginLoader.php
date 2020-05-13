5f452d239a8ff5b0b2b66825e1d2806882408e0b
Jacob Mansfield <cyberjacob@gmail.com>
2020-05-13 15:44:11 +0100

    Update PluginLoader.php

--- incl/PluginLoader.php
+++ incl/PluginLoader.php
@@ -7,31 +7,16 @@
  * LICENSE: This source file is subject to version 3.0 of the GNU General
  * Public License v3.0 that is attached to this project.
  *
- * @author     Marc Ole Bulling
- * @copyright  2019 Marc Ole Bulling
- * @license    https://www.gnu.org/licenses/gpl-3.0.en.html  GNU GPL v3.0
- * @since      File available since Release 1.2
+ * @author    Marc Ole Bulling
+ * @copyright 2019 Marc Ole Bulling
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html  GNU GPL v3.0
+ * @since     File available since Release 1.2
  */
-
-
-/**
- * Loader class for plugins
- *
- * @author     Marc Ole Bulling
- * @copyright  2019 Marc Ole Bulling
- * @license    https://www.gnu.org/licenses/gpl-3.0.en.html  GNU GPL v3.0
- * @since      File available since Release 1.2
- */
-
 
 $LOADED_PLUGINS = array();
-
 
 //Load all PHP files placed in the "plugins" folder
 foreach (glob(__DIR__ . "/../plugins/*.php") as $filename) {
     array_push($LOADED_PLUGINS, pathinfo($filename)["filename"]);
     include_once $filename;
 }
-
-
-?>
