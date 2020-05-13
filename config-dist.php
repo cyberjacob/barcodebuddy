d27e560ea884c09591aaeb04bdc3d39db7379a85
Jacob Mansfield <cyberjacob@gmail.com>
2020-05-13 13:19:09 +0100

    Update config-dist.php

--- config-dist.php
+++ config-dist.php
@@ -8,10 +8,10 @@
  * LICENSE: This source file is subject to version 3.0 of the GNU General
  * Public License v3.0 that is attached to this project.
  *
- * @author     Marc Ole Bulling
- * @copyright  2019 Marc Ole Bulling
- * @license    https://www.gnu.org/licenses/gpl-3.0.en.html  GNU GPL v3.0
- * @since      File available since Release 1.0
+ * @author    Marc Ole Bulling <Forceu@users.noreply.github.com>
+ * @copyright 2019 Marc Ole Bulling
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html  GNU GPL v3.0
+ * @since     File available since Release 1.0
  *
  *
  * Config file for Barcode Buddy. These configurations cannot be changed
